825b769e8b2593c0a2dcc8cfa7648820987dabe6
Mark Lee <mark.lee@capricasoftware.co.uk>
2023-06-16 12:18:37 +0100

    feat(ui): add MUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/mui'
+
 export default function Home() {
     return (
         <main>
             <h1>Ready to go...</h1>
+            <Button variant='contained' color='primary'>
+                I am button
+            </Button>
         </main>
     )
 }
---
+++ src/components/mui/index.ts
@@ -0,0 +1,10 @@
+/**
+ * MUI does not (yet) support React Server Components so we use this compatibility layer to use MUI
+ * components as client components.
+ */
+
+'use client'
+
+import { Button } from '@mui/material'
+
+export { Button }
